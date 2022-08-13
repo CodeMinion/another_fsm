84b9823758e292496ab97141d6a671c5b7c8dcf3
Frank Hernandez <codeminion@Franks-MacBook-Air.local>
2022-08-13 13:24:05 -0400

    Updated to ensure sequencing

--- lib/another_fsm.dart
+++ lib/another_fsm.dart
@@ -1,8 +1,11 @@
 library another_fsm;
 
+import 'dart:async';
+import 'dart:collection';
 
-abstract class FsmOwner{
+abstract class FsmOwner {
   Fsm? getFsm();
+
   Future<bool> handleEvent({required FsmEvent event});
 }
 
@@ -12,7 +15,6 @@ abstract class FsmEvent {}
 /// Represents a single state in the FSM. The logic for that state
 /// will be contained here.
 abstract class FsmState {
-
   /// Called when transitioning into the state.
   Future<void> onEnter({required FsmOwner owner});
 
@@ -24,7 +26,6 @@ abstract class FsmState {
 
   /// Called when an event is received by the FSM
   Future<bool> onEvent({required FsmEvent event, required FsmOwner owner});
-
 }
 
 /// Handles the state transitions and forwaring of events to
@@ -32,24 +33,77 @@ abstract class FsmState {
 class Fsm {
   final FsmOwner _owner;
   FsmState? _currentState;
+  final Queue _transactions = Queue<_FsmTransaction>();
 
-  Fsm({required FsmOwner owner}):_owner = owner;
+  Fsm({required FsmOwner owner}) : _owner = owner;
 
   /// Transitions the FSM to the next state and exist the previous one.
   Future<void> changeState({FsmState? nextState}) async {
-    _currentState?.onExit(owner: _owner);
+    // Note: This needs to fully complete before we handle any events.
+    Completer completer = Completer();
+    _FsmTransaction transaction = _FsmTransaction<void, FsmState>(name: "TransChangeState-$_currentState-$nextState",action: _changeState, completer: completer, value: nextState);
+    await _scheduleTransaction(transaction: transaction);
+    return completer.future;
+  }
+
+  Future<void> _changeState(FsmState? nextState) async {
+    // Note: This needs to fully complete before we handle any events.
+    await _currentState?.onExit(owner: _owner);
     _currentState = nextState;
-    _currentState?.onEnter(owner: _owner);
+    await nextState?.onEnter(owner: _owner);
   }
 
   Future<void> update({required int time}) async {
+    // TODO Add transaction on update too.
     await _currentState?.onUpdate(owner: _owner, time: time);
   }
 
   /// Send event to current state for handling.
   Future<bool> handleEvent({required FsmEvent event}) async {
-    return await _currentState?.onEvent(event:event, owner: _owner) ?? false;
+    Completer<bool> completer = Completer();
+    _FsmTransaction transaction = _FsmTransaction<bool, FsmEvent>( name: "TransHandleEvent-$event", action:_handleEvent, completer: completer, value: event);
+    await _scheduleTransaction(transaction: transaction);
+    return completer.future;
+  }
+
+  Future<bool> _handleEvent(FsmEvent event) async {
+    return await getCurrentState()?.onEvent(event: event, owner: _owner) ?? false;
   }
 
   FsmState? getCurrentState() => _currentState;
+
+  Future<void> _scheduleTransaction({required _FsmTransaction transaction}) async{
+    bool empty = _transactions.isEmpty;
+    _transactions.addLast(transaction);
+    if (empty) {
+      _executeNext();
+    }
+  }
+
+  Future<void> _executeNext() async {
+    if (_transactions.isNotEmpty) {
+      _FsmTransaction next = _transactions.first;
+      next.execute().then((value) {
+        _transactions.removeFirst();
+          _executeNext();
+      });
+    }
+  }
+}
+
+class _FsmTransaction<T,V> {
+  String name;
+  Future<T> Function(V value) action;
+  Completer<T> completer;
+  dynamic value;
+  _FsmTransaction({required this.name, required this.action, required this.completer, this.value});
+
+  Future execute() async {
+    T result;
+    result = await action(value);
+    completer.complete(result);
+  }
+
+  @override
+  String toString() => name;
 }
